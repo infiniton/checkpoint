dfeab42690d3ee3c8cbd690a2829a2d5fa9dd094
Alec <alecagayan24@gmail.com>
2022-07-20 19:55:44 -0400

    Initial Commit

---
+++ Shared/checkpointApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkpointApp.swift
+//  Shared
+//
+//  Created by Alec Agayan on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct checkpointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
